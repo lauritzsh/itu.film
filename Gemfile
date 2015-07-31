5ebf9ab0ae27c5a05e51ffe8c3a88366ae22b290
Lauritz Hilsøe <mail@lauritz.me>
2015-07-31 21:25:51 +0200

    Add production conditionals; remove config.rb

--- Gemfile
+++ Gemfile
@@ -2,6 +2,5 @@ source "https://rubygems.org"
 
 gem 'compass', '~> 1.0.3'
 gem 'susy', '~> 2.2.2'
-gem 'autoprefixer-rails', '~> 5.1.8'
 gem 'scss-lint', '~> 0.36.1'
 
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,56 +1,66 @@
 import del from 'del';
-import path from 'path';
 import sequence from 'run-sequence';
 import gulp from 'gulp';
 import compass from 'gulp-compass';
+import gif from 'gulp-if';
 import minifyCSS from 'gulp-minify-css';
 import minifyHTML from 'gulp-minify-html';
 import pages from 'gulp-gh-pages';
 import replace from 'gulp-replace';
 import uglify from 'gulp-uglify';
+import autoprefixer from 'gulp-autoprefixer';
+
+let isProduction = process.env.NODE_ENV === 'production';
+const BUILD_FOLDER = isProduction ? 'dist' : 'build';
 
 function prefix(path) {
-  const escaped = path.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+  const escaped = path.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
   return replace(new RegExp(escaped, 'g'), `${path}itu.film/`);
 }
 
 gulp.task('clean', (cb) => {
-  return del('dist', cb);
+  return del(BUILD_FOLDER, cb);
 });
 
 gulp.task('sass', () => {
+  const options = {
+    css: `${BUILD_FOLDER}/css`,
+    require: ['susy'],
+  };
+
+  if (!isProduction) { options.sourcemap = true; }
+
   return gulp.src('sass/**/*.scss')
-    .pipe(compass({
-      config_file: 'config.rb'
-    }))
-    .pipe(prefix('url("/'))
-    .pipe(minifyCSS())
-    .pipe(gulp.dest('dist/css'));
+    .pipe(compass(options))
+    .pipe(autoprefixer())
+    .pipe(gif(isProduction, prefix('url("/')))
+    .pipe(gif(isProduction, minifyCSS()))
+    .pipe(gulp.dest(`${BUILD_FOLDER}/css`));
 });
 
 gulp.task('html', () => {
   return gulp.src('{.,events,search,who}/index.html')
-    .pipe(prefix('src="/'))
-    .pipe(prefix('href="/'))
-    .pipe(prefix("Audio('/"))
-    .pipe(minifyHTML())
-    .pipe(gulp.dest('dist'));
+    .pipe(gif(isProduction, prefix('src="/')))
+    .pipe(gif(isProduction, prefix('href="/')))
+    .pipe(gif(isProduction, prefix("Audio('/")))
+    .pipe(gif(isProduction, minifyHTML()))
+    .pipe(gulp.dest(BUILD_FOLDER));
 });
 
 gulp.task('js', () => {
   return gulp.src('js/**')
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gif(isProduction, uglify()))
+    .pipe(gulp.dest(`${BUILD_FOLDER}/js`));
 });
 
 gulp.task('images', () => {
   return gulp.src('images/**')
-    .pipe(gulp.dest('dist/images'));
+    .pipe(gulp.dest(`${BUILD_FOLDER}/images`));
 });
 
 gulp.task('audio', () => {
   return gulp.src('audio/**')
-    .pipe(gulp.dest('dist/audio'));
+    .pipe(gulp.dest(`${BUILD_FOLDER}/audio`));
 });
 
 gulp.task('build', (cb) => {
@@ -58,6 +68,8 @@ gulp.task('build', (cb) => {
 });
 
 gulp.task('pages', ['build'], () => {
+  isProduction = true;
+
   return gulp.src('dist/**')
     .pipe(pages());
 });
